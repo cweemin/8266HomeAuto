708db6209e5c5a020f1f53aeb994fc84901c1b48
Wee-Min Chan <cweemin@gmail.com>
2017-08-14 20:30:04 -0700

    Updated to add label.
    - Add label
    - Add Node project for web

--- switch.cpp
+++ switch.cpp
@@ -13,11 +13,11 @@ void switch_prop::set() {
   _rcswitch->setPulseLength(_nPulseLength);
 }
 
-pSwitch::pSwitch(switch_prop *tmp_switch_prop, unsigned long on_code, unsigned long off_code) {
+pSwitch::pSwitch(switch_prop *tmp_switch_prop, unsigned long on_code, unsigned long off_code, String tmp) {
    _on_code = on_code;
    _off_code  = off_code;
    _switch_prop = tmp_switch_prop;
-
+   label = tmp;
 }
 
 void pSwitch::Toggle(bool onOff) {
@@ -29,10 +29,12 @@ void pSwitch::Toggle(bool onOff) {
 void pSwitch::On(void) {
    _switch_prop->set();
    _switch_prop->_rcswitch->send(_on_code, _switch_prop->_length);
+//   ws->textAll(label+ " On");
 }
 
 void pSwitch::Off(void) {
    _switch_prop->set();
    //TODO: Need to pause receiver
    _switch_prop->_rcswitch->send(_off_code, _switch_prop->_length);
+//   ws->textAll(label+ " Off");
 }
